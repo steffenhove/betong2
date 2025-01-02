1b2e834f9a4af894821d684be6e858f00848622c
steffenhove <steffenhove@gmail.com>
2025-01-02 21:51:07 +0100

    Update Calculation.kt

--- main/java/no/steffenhove/betongkalkulator/Calculation.kt
+++ main/java/no/steffenhove/betongkalkulator/Calculation.kt
@@ -1,10 +1,16 @@
 package no.steffenhove.betongkalkulator
 
-data class Calculation(
-    val volume: Double,
-    val weight: Double,
-    val shape: String,
-    val dimensions: String,
-    val datetime: String,
-    val unitSystem: String
-)
+fun calculateCylinderVolume(diameter: Double, height: Double): Double {
+    val radius = diameter / 2
+    return Math.PI * radius * radius * height
+}
+
+fun calculateCuboidVolume(length: Double, width: Double, thickness: Double): Double {
+    return length * width * thickness
+}
+
+fun calculateTriangleVolume(a: Double, b: Double, c: Double, thickness: Double): Double {
+    val s = (a + b + c) / 2
+    val area = Math.sqrt(s * (s - a) * (s - b) * (s - c))
+    return area * thickness
+}
