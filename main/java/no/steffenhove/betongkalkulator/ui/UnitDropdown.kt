0604349900536f29b84c73ac637a987cc4e56a55
steffenhove <steffenhove@gmail.com>
2025-01-02 20:51:31 +0100

    Update UnitDropdown.kt

--- main/java/no/steffenhove/betongkalkulator/ui/UnitDropdown.kt
+++ main/java/no/steffenhove/betongkalkulator/ui/UnitDropdown.kt
@@ -5,6 +5,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment // Importer Alignment
 import no.steffenhove.betongkalkulator.Unit
 
 @Composable
