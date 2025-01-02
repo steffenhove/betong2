1b7787f2cb2bf35e5b7a57f8a0d9a71205b74ab3
steffenhove <steffenhove@gmail.com>
2025-01-02 20:44:26 +0100

    Update TrekantInput.kt

--- main/java/no/steffenhove/betongkalkulator/ui/TrekantInput.kt
+++ main/java/no/steffenhove/betongkalkulator/ui/TrekantInput.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import no.steffenhove.betongkalkulator.UnitDropdown
+import no.steffenhove.betongkalkulator.Unit
 
 @Composable
 fun TrekantInput(onCalculateClick: (String, String, String, String, Unit, Unit, Unit, Unit) -> Unit) {
@@ -44,11 +44,11 @@ fun TrekantInput(onCalculateClick: (String, String, String, String, Unit, Unit, 
             label = { Text("Tykkelse") },
             modifier = Modifier.fillMaxWidth()
         )
-        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()){
-            UnitDropdown(selectedUnit = selectedAUnit){ selectedAUnit = it }
-            UnitDropdown(selectedUnit = selectedBUnit){ selectedBUnit = it }
-            UnitDropdown(selectedUnit = selectedCUnit){ selectedCUnit = it }
-            UnitDropdown(selectedUnit = selectedThicknessUnit){ selectedThicknessUnit = it }
+        Row(horizontalArrangement = Arrangement.SpaceEvenly, modifier = Modifier.fillMaxWidth()) {
+            UnitDropdown(selectedUnit = selectedAUnit) { selectedAUnit = it }
+            UnitDropdown(selectedUnit = selectedBUnit) { selectedBUnit = it }
+            UnitDropdown(selectedUnit = selectedCUnit) { selectedCUnit = it }
+            UnitDropdown(selectedUnit = selectedThicknessUnit) { selectedThicknessUnit = it }
         }
         Spacer(modifier = Modifier.height(16.dp))
         Button(onClick = {
