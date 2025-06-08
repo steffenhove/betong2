211e363fcf0e5aa896d7b29d4a07c52e7e07b03a
steffenhove <steffenhove@gmail.com>
2025-06-08 16:25:38 +0200

    Delete main directory

--- main/java/no/steffenhove/betongkalkulator/ui/model/OverskjaeringData.kt
+++
@@ -1,8 +0,0 @@
-package no.steffenhove.betongkalkulator.ui.model
-
-// Denne representerer alle datapunktene for én bladdiameter
-data class OverskjaeringData(
-    val bladeSize: Int,
-    // Map fra Betongtykkelse (Int) til et par med (Overkapp, Skjæredybde)
-    val dataPoints: Map<Int, Pair<Float, Float>>
-)
--- main/java/no/steffenhove/betongkalkulator/ui/model/ThicknessValues.kt
+++
@@ -1,9 +0,0 @@
-package no.steffenhove.betongkalkulator.ui.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ThicknessValues(
-    @SerializedName("minCutCm") val minCutCm: Float,
-    @SerializedName("maxCutCm") val maxCutCm: Float,
-    @SerializedName("overcutCm") val overcutCm: Float
-)
